a0edc682baa2c2778e91796824257478d0c1974d
Prranith <160862370+Prranith@users.noreply.github.com>
2025-01-04 01:15:00 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,13 +4,13 @@ import firestore from '@react-native-firebase/firestore';
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBdvx2QKymofjb_XoMMpKzufxqq17L03so",
-  authDomain: "growsmart-aa367.firebaseapp.com",
-  projectId: "growsmart-aa367",
-  storageBucket: "growsmart-aa367.appspot.com",
+  apiKey: "your_api key",
+  authDomain: "---",
+  projectId: "----",
+  storageBucket: "---",
   messagingSenderId: "62665875649",
-  appId: "1:62665875649:android:c7690f7cac972b23e67440",
-  measurementId: "G-XXXXXXXXXX",
+  appId: "---",
+  measurementId: "---",
 };
 
 // Initialize Firebase if it hasn't been initialized already
